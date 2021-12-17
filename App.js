b0b2e25737f61a06f288571eb6c3e29e832e18e5
Ogola <90789301+nonu0@users.noreply.github.com>
2021-12-18 00:58:32 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import './App.css'
+import Numbers from './components/Numbers';
+function App() {
+  return (
+    <div className='container'>
+    <Numbers/>
+   </div>
+  );
+}
+
+export default App;
